b8c42f7d477739a5369782b75ba2dda5cc9d1ca6
Michael Tran <michael.tran2208@gmail.com>
2021-03-16 18:31:37 +1100

    initial commit

---
+++ src/main/java/com/tran/ProfessionalDashboard/ProfessionalDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.tran.ProfessionalDashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessionalDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessionalDashboardApplication.class, args);
+	}
+
+}
