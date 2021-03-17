a9b5b6f92867489f99615631e38fd12fefe6dda2
Michael Tran <michael.tran2208@gmail.com>
2021-03-17 14:15:28 +1100

    implemented get request using fetch

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
-import { Button } from "antd";
+import { Button, Radio } from "antd";
 
 function App() {
   return (
     <div className="App">
         <Button type='primary'>Hello</Button>
+        <br/>
+
+        <Radio.Group value='large' >
+          <Radio.Button value="large">Large</Radio.Button>
+          <Radio.Button value="default">Default</Radio.Button>
+          <Radio.Button value="small">Small</Radio.Button>
+        </Radio.Group>
     </div>
   );
 }
---
+++ src/frontend/src/client.js
@@ -0,0 +1,16 @@
+import fetch from 'unfetch';
+
+const checkStatus = response => {
+  if (response.ok) {
+    return response;
+  }
+  // convert non-2xx HTTP responses into errors:
+  const error = new Error(response.statusText);
+  error.response = response;
+  return Promise.reject(error);
+}
+
+export const getAllStudents = () => {
+  fetch('localhost:8080/api/v1/students')
+    .then(checkStatus)
+}
