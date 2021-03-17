6587cfdefb7793287d1a47687749d09af3e256f0
Michael Tran <michael.tran2208@gmail.com>
2021-03-17 13:54:01 +1100

    bootstrapped react frontend

---
+++ src/frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Button } from "antd";
+
+function App() {
+  return (
+    <div className="App">
+        <Button type='primary'>Hello</Button>
+    </div>
+  );
+}
+
+export default App;
