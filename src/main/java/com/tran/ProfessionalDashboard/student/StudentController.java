1b94345b194e4d7ab6f81990bcfd8b45b87e6adb
Michael Tran <michael.tran2208@gmail.com>
2021-03-24 16:35:18 +1100

    added n-tier student architecture

--- src/main/java/com/tran/ProfessionalDashboard/student/StudentController.java
+++ src/main/java/com/tran/ProfessionalDashboard/student/StudentController.java
@@ -1,5 +1,6 @@
 package com.tran.ProfessionalDashboard.student;
 
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,15 +10,14 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/students")
+@AllArgsConstructor
 public class StudentController {
+
+    private final StudentService studentService;
 
     @GetMapping
     public List<Student> getAllStudents() {
-        List<Student> students = Arrays.asList(
-                new Student(1L, "jamila", "jamilia@amigoscode.edu", Gender.FEMALE),
-                new Student(2L, "alex", "alex@amigoscode.edu", Gender.MALE)
-        );
 
-        return students;
+        return studentService.getAllStudents();
     }
 }
